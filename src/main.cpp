a5cdbd6921abed3448f473f41f34135405969dfe
stanislaumich <staslz42stas@gmail.com>
2019-08-23 21:58:20 +0300

    Почистил, вынес мусор, оставил голый скелет

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,92 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+// All the mcufriend.com UNO shields have the same pinout.
+// i.e. control pins A0-A4.  Data D2-D9.  microSD D10-D13.
+// Touchscreens are normally A1, A2, D7, D6 but the order varies
+//
+// This demo should work with most Adafruit TFT libraries
+// If you are not using a shield,  use a full Adafruit constructor()
+// e.g. Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
+
+#define LCD_CS A3 // Chip Select goes to Analog 3
+#define LCD_CD A2 // Command/Data goes to Analog 2
+#define LCD_WR A1 // LCD Write goes to Analog 1
+#define LCD_RD A0 // LCD Read goes to Analog 0
+#define LCD_RESET A4 // Can alternately just connect to Arduino's reset pin
+
+// my biggest screen is 320x480
+
+#include <SPI.h>          // f.k. for Arduino-1.5.2
+#include "Adafruit_GFX.h"// Hardware-specific library
+#include <MCUFRIEND_kbv.h>
+MCUFRIEND_kbv tft;
+//#include <Adafruit_TFTLCD.h>
+//Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
+
+// Assign human-readable names to some common 16-bit color values:
+#define	BLACK   0x0000
+#define	BLUE    0x001F
+#define	RED     0xF800
+#define	GREEN   0x07E0
+#define CYAN    0x07FF
+#define MAGENTA 0xF81F
+#define YELLOW  0xFFE0
+#define WHITE   0xFFFF
+
+#ifndef min
+#define min(a, b) (((a) < (b)) ? (a) : (b))
+#endif
+
+void setup(void);
+void loop(void);
+/*unsigned long testFillScreen();
+unsigned long testText();
+unsigned long testLines(uint16_t color);
+unsigned long testFastLines(uint16_t color1, uint16_t color2);
+unsigned long testRects(uint16_t color);
+unsigned long testFilledRects(uint16_t color1, uint16_t color2);
+unsigned long testFilledCircles(uint8_t radius, uint16_t color);
+unsigned long testCircles(uint8_t radius, uint16_t color);
+unsigned long testTriangles();
+unsigned long testFilledTriangles();
+unsigned long testRoundRects();
+unsigned long testFilledRoundRects();
+void progmemPrint(const char *str);
+void progmemPrintln(const char *str);
+
+void runtests(void);
+
+uint16_t g_identifier;
+*/
+
+void setup(void) {
+    Serial.begin(9600);
+    //    tft.reset();                 //hardware reset
+    uint16_t ID = tft.readID(); //
+    Serial.print("ID = 0x");
+    Serial.println(ID, HEX);
+    tft.begin(ID);  // my is 9327
+    tft.fillScreen(BLACK);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void myfunc1(void)
+{
+ tft.fillScreen(BLACK);
 }
+
+
+
+void printmsg(int row, const char *msg)
+{
+    tft.setTextColor(YELLOW, BLACK);
+    tft.setCursor(0, row);
+    tft.println(msg);
+}
+
+void loop(void) {
+    myfunc1();
+    tft.fillScreen(BLACK);
+    delay(1000);
+    
+}
+
